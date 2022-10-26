93512c72fb2d78cf25bd2ea46eae60b6d54f9f6d
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2022-10-25 20:19:04 -0400

    Aula 2

---
+++ 2/main.go
@@ -0,0 +1,22 @@
+package main
+
+// const a = "Hello world!"
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "Hello world!"
+	e float64 = 1.2
+)
+
+func main() {
+	// b = true
+	a := "My name is"
+	a = "Your name is"
+	println(a)
+	println(b)
+	println(c)
+	println(d)
+	println(e)
+
+}
