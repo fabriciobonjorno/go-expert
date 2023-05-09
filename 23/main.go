590b7330e497c312cfc2fa8345b71c3cf0e0b1d7
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2023-05-09 12:45:58 -0400

    Aulas go fundação

---
+++ 23/main.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+
+	for i := 0; i < 10; i++ {
+		println(i)
+	}
+
+	numeros := []string{"um", "dois", "tres"}
+
+	for k, v := range numeros {
+		println(k, v)
+	}
+
+	for _, v := range numeros {
+		println(v)
+	}
+
+	i := 0
+	for i < 10 {
+		println(i)
+		i++
+	}
+}
