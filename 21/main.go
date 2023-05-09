ba59a0789caaf555c691cbe12ef09b7becbb7039
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2023-05-08 22:05:53 -0400

    Aulas go fundação

--- 21/main.go
+++ 21/main.go
@@ -8,6 +8,9 @@ import (
 
 func main() {
 	s := matematica.Soma(10, 20)
+	carro := matematica.Carro{Marca: "Fiat"}
+	fmt.Println(carro.Andar())
 	fmt.Println("O resultado: %v", s)
+	fmt.Println(matematica.A)
 
 }
